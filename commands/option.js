f3d4f6fb8184fcae778d0a41c9b12c57722a58ce
Yosuke Yamakawa <tech@y-yamakawa.com>
2021-12-30 18:04:14 +0900

    First commit

---
+++ commands/option.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { exec } = require('child_process');
+const { stat } = require('fs');
+const dotenv = require('dotenv');
+const execSync = require('child_process').execSync;
+const { escape } = require('querystring');
+let status;
+
+function delay(n){ //非同期のdelayを定義
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+
+let Embed = new MessageEmbed()
+.setColor('#0099ff')
+.setTitle('Server Start')
+.setDescription('サーバーを起動しています');
+
+module.exports = {
+    data: {
+        name: "start",
+        description: "サーバーを起動します",
+        options: [{
+            type: "STRING",
+            name: "server",
+            description: "起動するサーバーを入力してください",
+            required: true,
+            choices: [
+                { name: "dev1", value: "dev1" },
+                { name: "dev2", value: "dev2" },
+                { name: "dev3", value: "dev3" },
+            ]
+        }],
+    },
+    async execute(interaction) {
+        if (interaction.options.getString('server') === "dev1") {
+            await interaction.reply({ embeds: [Embed] });
+            block: for (;;){
+                execSync(`aws ec2 --profile ap-northeast-1 start-instances --instance-ids ${process.env.DEV1_ID}`).toString();
+                status = execSync(`aws ec2 --profile ap-northeast-1 describe-instances --instance-id ${process.env.DEV1_ID} --query Reservations[*].Instances[*].State.Name --output text`).toString();
+                status = status.replace(/\r?\n/g,'');
+                addres = execSync(`aws ec2 --profile ap-northeast-1 describe-instances --instance-id ${process.env.DEV1_ID} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text`).toString();
+                console.log(status);
+                let Embed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`${interaction.options.getString('server')} start`)
+                .addField('Status', status)
+                .addField('Addres', addres)
+                interaction.editReply({ embeds: [Embed] });
+                if (status == "running"){
+                    break block;
+                }
+                await delay(1)
+            }
+        }
+        else if (interaction.options.getString('server') === "dev2") {
+            await interaction.reply('サーバーDev2を起動します');
+        }
+        else if (interaction.options.getString('server') === "dev3") {
+            await interaction.reply('サーバーDev3を起動します');
+        }
+        else {
+            await interaction.reply('正しいサーバー名を入力してください');
+        }
+    }
+}
