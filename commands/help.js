ad172b977321ca1580950c9e64958e9a47f1af3e
Yosuke Yamakawa <tech@y-yamakawa.com>
2021-12-30 23:44:39 +0900

    help.jsのembedを追記

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,16 @@
 const { MessageEmbed } = require('discord.js');
 
 const Embed = new MessageEmbed()
+	.setTitle('aws_discord_bot')
+	.setDescription('これはDiscord.jsを用いてAWS CLIを操作するBotプログラムです')
+	.setURL('https://github.com/Honahuku/aws_discord_bot')
+    .setThumbnail('')
 	.setColor('#0099ff')
-	.setTitle('help')
-	.setDescription('このBotのヘルプページを表示します');
+	.addField('利用可能なコマンド', 'a')
+	.addField('info', 'サーバーの状態を取得します')
+	.addField('start', 'サーバーを起動します')
+	.addField('stop', 'サーバーを停止します')
+	.addField('help', 'このヘルプページを表示します');
 
 module.exports = {
 
